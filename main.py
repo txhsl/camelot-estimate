f3898bd6ce0ba13407497962413294321809bb90
txhsl <799498265@qq.com>
2023-05-25 18:02:04 +0800

    update

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import math
 
 # reserves
 print("Params input:")
-xr = 1472667531251509966538 # WETH
-yr = 647408399605644450831 # WstETH
+xr = 1472931579867829803872 # WETH
+yr = 647174395763910002518 # WstETH
 
 # in-hand amounts
-x_in = 100 * 10 ** 18
+x_in = 1 * 10 ** 18
 y_in = 0
 print("xr = " + str(xr / 10 ** 18) + ", yr = " + str(yr / 10 ** 18) + ", x_in = " + str(x_in / 10 ** 18) + ", y_in = " + str(y_in / 10 ** 18))
 
@@ -62,8 +62,8 @@ print("y = " + str(y1 / 10 ** 18))
 assert(math.fabs((x_in - (x1 - xr)) / (y_in + (yr - y1)) - (x1 / y1)) < 10 ** -6)
 
 # compare between with fee and without fee
-real_delta_y = 28.004410421688815616
+real_delta_y = 0.293721133620124416
 print("Compare between with fee and without fee:")
 print("delta_x = " + str((x1 - xr) / 10 ** 18))
-print("delta_y = " + str((yr - y1) / 10 ** 18))
+print("delta_y = " + str((y1 - yr) / 10 ** 18))
 print("But the real 'delta_y' we can get: " + str(real_delta_y)) 
